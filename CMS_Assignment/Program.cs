e7072b8b7960e5b2e0026f46da67bf4c3925bf15
feliciaemerentcia <feliciaemerentcia@gmail.com>
2025-11-15 19:39:25 +0100

    first commit

---
+++ CMS_Assignment/Program.cs
@@ -0,0 +1,32 @@
+using CMS_Assignment.Services;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddScoped<FormSubmissionsService>();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
---
+++ CMS_Assignment/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS_Assignment.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^(?=.{1,254}$)(?=.{1,64}@)[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[A-Za-z]{2,}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Phone is required")]
+    [Display(Name = "Phone")]
+    public string Phone {  get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select an option")]
+    public string SelectedOption { get; set; } = null!;
+}
